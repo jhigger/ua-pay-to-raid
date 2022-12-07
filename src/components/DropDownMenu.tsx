4d1c210e13290232d4ada8a21c868f239afb28eb
jhigger <njohnjhigger@gmail.com>
2022-12-08 05:44:23 +0800

    change to hover

--- src/components/DropDownMenu.tsx
+++ src/components/DropDownMenu.tsx
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-
 interface Props {
 	//boolean to always open ddm (for presentation)
 	forceOpen?: boolean;
@@ -18,13 +16,13 @@ export interface DDMItem {
 }
 
 const DropDownMenu = (props: Props) => {
-	const [isOpen, setIsOpen] = useState(false);
+	// const [isOpen, setIsOpen] = useState(true);
 	return (
-		<div className="relative inline-block text-left">
+		<div className="group relative inline-block text-left">
 			<div>
 				<button
 					type="button"
-					onClick={() => setIsOpen(!isOpen)}
+					// onClick={() => setIsOpen(!isOpen)}
 					className={` ${
 						props.withBackground
 							? "border border-gray-300 bg-white shadow-sm dark:bg-gray-800"
@@ -48,44 +46,40 @@ const DropDownMenu = (props: Props) => {
 				</button>
 			</div>
 
-			{(props.forceOpen || isOpen) && (
-				<div className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 dark:bg-gray-800">
-					<div
-						className={`py-1 ${
-							props.withDivider ? "divide-y divide-gray-100" : ""
-						}`}
-						role="menu"
-						aria-orientation="vertical"
-						aria-labelledby="options-menu"
-					>
-						{props.items.map((item) => {
-							return (
-								<a
-									key={item.label}
-									href={item.link || "#"}
-									className={`${
-										item.icon
-											? "flex items-center"
-											: "block"
-									} text-md block px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:bg-gray-600 dark:hover:text-white`}
-									role="menuitem"
-								>
-									{item.icon}
+			<div className="absolute right-0 mt-0 hidden w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 group-hover:block dark:bg-gray-800">
+				<div
+					className={`py-1 ${
+						props.withDivider ? "divide-y divide-gray-100" : ""
+					}`}
+					role="menu"
+					aria-orientation="vertical"
+					aria-labelledby="options-menu"
+				>
+					{props.items.map((item) => {
+						return (
+							<a
+								key={item.label}
+								href={item.link || "#"}
+								className={`${
+									item.icon ? "flex items-center" : "block"
+								} text-md block px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:bg-gray-600 dark:hover:text-white`}
+								role="menuitem"
+							>
+								{item.icon}
 
-									<span className="flex flex-col">
-										<span>{item.label}</span>
-										{item.desc && (
-											<span className="text-xs text-gray-400">
-												{item.desc}
-											</span>
-										)}
-									</span>
-								</a>
-							);
-						})}
-					</div>
+								<span className="flex flex-col">
+									<span>{item.label}</span>
+									{item.desc && (
+										<span className="text-xs text-gray-400">
+											{item.desc}
+										</span>
+									)}
+								</span>
+							</a>
+						);
+					})}
 				</div>
-			)}
+			</div>
 		</div>
 	);
 };
